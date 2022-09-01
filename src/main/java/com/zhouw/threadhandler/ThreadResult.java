7dc01d00cb0f2777d2f086e310748f7f0c93d431
1300012858 <1300012858@qq.com>
2022-09-01 15:57:50 +0800

    init

---
+++ src/main/java/com/zhouw/threadhandler/ThreadResult.java
@@ -0,0 +1,19 @@
+package com.zhouw.threadhandler;
+
+public class ThreadResult<T>{
+    private ThreadTask thread;
+    private T data;
+
+    public ThreadResult(ThreadTask thread, T data) {
+        this.thread = thread;
+        this.data = data;
+    }
+
+    public ThreadTask getThread() {
+        return thread;
+    }
+
+    public T getData() {
+        return data;
+    }
+}
